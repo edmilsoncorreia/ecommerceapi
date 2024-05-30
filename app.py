86ae6b45b5f3cd87ec19f83ef3b7fe7a8f7b883a
Edmilson Correia <edmilson.cc@gmail.com>
2024-05-30 05:33:43 -0300

    create import flask

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 #Definir uma rota raiz (pagina inicial) e a função que será realizada ao requisitar
+@app.route('/')
+
+def hello_world():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(debug=True)
