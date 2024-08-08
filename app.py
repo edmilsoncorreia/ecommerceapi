ff0a48bbdece08b0f0f0b4c33e3139fe386c21b6
Edmilson Correia <edmilson.cc@gmail.com>
2024-08-08 04:29:39 -0300

    updating

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 #IMPORTAÇÃO
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+db = SQLAlchemy(app)
+
+#MODELAGEM
+#Produto (id, name, price, description)
+
+class  Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 #Definir uma rota raiz (pagina inicial) e a função que será realizada ao requisitar
 @app.route('/')
